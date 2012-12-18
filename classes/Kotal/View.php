c3c5f1dbeefcae8ab3c1946d196b367783d23354
Drarok Ithaqua <source@catchall.drarok.com>
2012-12-18 04:05:04 +0000

    Avoid using TAL for the exception view.

--- classes/Kotal/View.php
+++ classes/Kotal/View.php
@@ -25,7 +25,7 @@ class Kotal_View extends Kohana_View {
 	 * @var array Cached list of excluded controllers
 	 */
 	protected static $_tal_exclude;
-	
+
 	/**
 	 * @var bool Cached global setting to enable tal on views.
 	 */
@@ -42,14 +42,19 @@ class Kotal_View extends Kohana_View {
 	public function __construct($file = NULL, array $data = NULL)
 	{
 		parent::__construct($file, $data);
-		
+
 		// Load the global setting if not already done.
 		if (self::$_tal_enable_default === NULL) {
 			self::$_tal_enable_default = Kohana::$config->load('kotal.enabled');
 		}
-		
-		// Use the global setting by default.
-		$this->use_tal(self::$_tal_enable_default);
+
+		if ($file == Kohana_Exception::$error_view) {
+			// Don't use TAL for the Exception view.
+			$this->use_tal(FALSE);
+		} else {
+			// Use the global setting by default.
+			$this->use_tal(self::$_tal_enable_default);
+		}
 	}
 
 	/**
@@ -204,7 +209,7 @@ class Kotal_View extends Kohana_View {
 			// Make sure the core of PHPTAL is loaded now, we'll need it.
 			require_once Kohana_Core::find_file('vendor', 'phptal/PHPTAL');
 		}
-		
+
 		return $this;
 	}
 
