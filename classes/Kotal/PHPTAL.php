743e52fbf34bbb7a3a7844db115cc60f2ed19f7e
Drarok Ithaqua <github@catchall.drarok.com>
2013-01-11 17:17:11 +0000

    Add docblocks.

--- classes/Kotal/PHPTAL.php
+++ classes/Kotal/PHPTAL.php
@@ -2,6 +2,12 @@
 
 class Kotal_PHPTAL extends PHPTAL
 {
+	/**
+	 * Overridden constructor, allowing a context to be passed in.
+	 *
+	 * @param mixed $path    Path, or FALSE to supply it later.
+	 * @param mixed $context PHPTAL_Context, or NULL for default.
+	 */
 	public function __construct($path = FALSE, $context = NULL)
 	{
 		parent::__construct($path);
@@ -11,6 +17,13 @@ class Kotal_PHPTAL extends PHPTAL
 		}
 	}
 
+	/**
+	 * Setter for the context.
+	 *
+	 * @param PHPTAL_Context $context New context.
+	 *
+	 * @return void
+	 */
 	public function setContext($context)
 	{
 		$this->_context = $context;
