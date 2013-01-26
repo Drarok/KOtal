687b6aa733eccceb44028edf1718629afe32f2f1
Drarok Ithaqua <source@catchall.drarok.com>
2013-01-26 19:03:01 +0000

    Add code for title and scripts/styles in a template.

--- classes/Kotal/Kotal/Layout.php
+++ classes/Kotal/Kotal/Layout.php
@@ -18,14 +18,61 @@ class Kotal_Kotal_Layout extends Kotal
 	protected $_layout = 'default';
 
 	/**
-	 * Page title, put into the <title> element.
+	 * Variables to pass into the layout.
 	 *
-	 * @var string
+	 * @var array
 	 */
-	protected $_title = '';
+	protected $_data = array(
+		'title'   => '',
+		'styles'  => array(),
+		'scripts' => array(),
+	);
 
 	/**
-	 * Render this view model, wrapped in a payout, and return its markup.
+	 * Add an extra stylesheet by name.
+	 *
+	 * Note that you must include the full path to your stylesheet, e.g.
+	 * $this->addStyle('/styles/extra.css');
+	 *
+	 * @param string $style Extra stylesheet to load into the layout.
+	 */
+	public function addStyle($style)
+	{
+		$this->_data['styles'][] = $style;
+		return $this;
+	}
+
+	/**
+	 * Add an extra script by name.
+	 *
+	 * Note that you must include the full path to your script, e.g.
+	 * $this->addStyle('/scripts/extra.js');
+	 *
+	 * @param string $script Extra script to load into the layout.
+	 */
+	public function addScript($script)
+	{
+		$this->_data['scripts'][] = $script;
+		return $this;
+	}
+
+	/**
+	 * Setter for layout variables.
+	 *
+	 * @param string $key   Name of the variable to set.
+	 * @param mixed  $value Value of the variable.
+	 *
+	 * @return Kotal_Layout
+	 * @chainable
+	 */
+	public function set($key, $value)
+	{
+		$this->_data[$key] = $value;
+		return $this;
+	}
+
+	/**
+	 * Render this view model, wrapped in a layout, and return its markup.
 	 *
 	 * @param mixed $template String template name, or NULL for default.
 	 *
@@ -35,7 +82,9 @@ class Kotal_Kotal_Layout extends Kotal
 	{
 		// This method uses PHPTAL directly. Possibly doesn't need to.
 		$tal = new Kotal_PHPTAL(Kohana::find_file('views', 'layouts/' . $this->_layout, 'xhtml'));
-		$tal->title = $this->_title;
+		foreach ($this->_data as $key => $value) {
+			$tal->$key = $value;
+		}
 		$tal->body = parent::render($template);
 		return '<!DOCTYPE html>' . $tal->execute();
 	}
